185b22fbfacc59c3941215937fee376a1f0813aa
Paulo Henrique <contactph51@gmail.com>
2024-09-09 01:19:25 -0300

    feat: add link to create a product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core'
+import { RouterLink } from '@angular/router'
+
+import { MatButtonModule } from '@angular/material/button'
+
+import { CardComponent } from './components/card/card.component'
 
 import { ProductsService } from '../../shared/services/products.service'
 
 import { Product } from '../../shared/interfaces/IProduct'
 
-import { CardComponent } from './components/card/card.component'
 
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
